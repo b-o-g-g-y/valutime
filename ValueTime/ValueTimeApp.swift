07a4e95cfdd8a23cfbdf23810d5e1a0e882a1fb9
b-o-g-g-y <support@shopndur.com>
2025-02-28 12:51:37 -0500

    Initial Commit

---
+++ ValueTime/ValueTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValueTimeApp.swift
+//  ValueTime
+//
+//  Created by Bogdan on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ValueTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ValueTimeTests/ValueTimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ValueTimeTests.swift
+//  ValueTimeTests
+//
+//  Created by Bogdan on 2/28/25.
+//
+
+import Testing
+@testable import ValueTime
+
+struct ValueTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
